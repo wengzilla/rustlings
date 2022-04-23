4a46d5779420dea631cd8653b040aa1d6a94bb2c
Edward Weng <edweng@gmail.com>
2022-04-23 01:12:15 -0700

    Finished up half of rustlings.

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -2,12 +2,11 @@
 // Make me compile without changing line 13!
 // Execute `rustlings hint move_semantics2` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
-    let vec0 = Vec::new();
+    let vec0: Vec<i32> = Vec::new();
+    let copy: Vec<i32> = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec(copy);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
@@ -19,6 +18,7 @@ fn main() {
 
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
     let mut vec = vec;
+  
 
     vec.push(22);
     vec.push(44);
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -7,10 +7,20 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn ..... {
+
+fn calculate_apple_price(qty: u32)  -> u32 {
+  let price: u32;
+
+  if qty > 40 {
+    price = 1;
+  } else {
+    price = 2;
+  }
+
+  return price * qty;
+}
+
 
 // Don't modify this function!
 #[test]
